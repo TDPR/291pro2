6c7c48007cc5294367065a879fb24da2f875b41a
Otman <omaoued1@gmail.com>
2018-11-15 17:24:43 -0700

    main files setup

---
+++ src/index.py
@@ -0,0 +1,30 @@
+def mainMenu():
+    print('Control the menu using numbers')
+    print('1. Prepare Data Files')
+    print('2. Build Indexes')
+    print('3. Retrieve Data')
+    print('4. Exit')
+    res=input()
+
+    if res == '1':
+        #call phase1.py
+        mainMenu()
+    elif res == '2':
+        #call phase2.py
+        mainMenu()
+    elif res == '3':
+        #call phase3.py
+        mainMenu()
+    elif res == '4':
+        print('\nGoodbye')
+        exit()
+    else:
+        print('\nInvalid Input')
+        mainMenu()
+
+
+if __name__ == '__main__':
+    print('Mini Project 2')
+    print('By maoued, sandare, tdr')
+
+mainMenu()
