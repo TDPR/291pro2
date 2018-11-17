2e1ecae7811a5092e24aaeb9a6c097445816d8e8
Otman <omaoued1@gmail.com>
2018-11-16 18:40:30 -0700

    parsing phase 1 complete

--- src/phase1.py
+++ src/phase1.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import xml.etree.ElementTree as ET
 
 fileName = open(str(sys.argv[1]),'r')
 
@@ -7,4 +8,37 @@ fileData = [line.rstrip('\n') for line in fileName]
 
 for line in fileData:
     if line.startswith('<ad>'):
-        print(line+'\n')
+        lineData = ET.fromstring(line)
+
+
+        stripTi = ""
+        for ch in lineData[4].text:
+            if re.match(r'([0-9a-zA-Z_-])+', ch):
+                stripTi += ch
+            else:
+                stripTi += ' '
+
+        stripTerms = ""
+        for ch in lineData[5].text:
+            if re.match(r'([0-9a-zA-Z_-])+', ch):
+                stripTerms += ch
+            else:
+                stripTerms += ' '
+
+        termsData = stripTi.split()
+        termsData.extend(stripTerms.split())
+        terms=[]
+        for word in termsData:
+            if len(word) > 2:
+                terms.append(':'.join([word.lower(),lineData[0].text]))
+
+        print(terms)
+
+        pdate = [lineData[1].text, lineData[0].text, lineData[3].text, lineData[2].text]
+        print(pdate)
+        prices = [':'.join([lineData[6].text,lineData[0].text]), lineData[3].text, lineData[2].text]
+        print(prices)
+        ads = [':'.join([lineData[0].text,line])]
+        print(ads)
+
+        print('\n')
