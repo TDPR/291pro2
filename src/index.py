babfff32690340f9aefc5662984c81df194e6ac2
Otman <omaoued1@gmail.com>
2018-11-17 10:07:04 -0700

    added index.py comments

--- src/index.py
+++ src/index.py
@@ -1,4 +1,6 @@
 #TODO phase1 and 2 don't need menus and can just be scripts
+#use this to copy into phase3.py when we get there
+#afterwards this can be deleted
 
 def mainMenu():
     print('Control the menu using numbers')
