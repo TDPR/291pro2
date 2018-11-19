5e1b5242e15101256365fb30e3f7fe6a78389f21
TDPR <ty.d.p.robinson@gmail.com>
2018-11-18 20:10:58 -0700

    added phase 2, needds to be tested on lab comps

---
+++ src/phase2.py
@@ -0,0 +1,27 @@
+import subprocess as sp
+import os
+
+# Unix command sort -u <filename> sorts the file and removes duplicates
+sp.call(['sort','-u','terms.txt'])
+sp.call(['sort','-u','pdates.txt'])
+sp.call(['sort','-u','prices.txt'])
+sp.call(['sort','-u','ads.txt'])
+
+#setting up each file for db_load, such that / are gone, key and data alternate lines
+filenames = ['terms.txt', 'pdates.txt','prices.txt','ads.txt']
+for name in filenames:
+    temp = open('temp.txt','w+')
+    fo = open(name,'r')
+    for line in fo:
+        sline = line.split(':')
+        temp.write(sline[0]+'\n')
+        temp.write(sline[1])
+    fo.close()
+    temp.close()
+    os.rename('temp.txt', name)
+
+# now using db_load for each file, assumes that Berkely db is installed
+sp.call(['db_load','-T','-t','btree','terms.txt'])
+sp.call(['db_load','-T','-t','btree','pdates.txt'])
+sp.call(['db_load','-T','-t','btree','prices.txt'])
+sp.call(['db_load','-T','-t','hash','ads.txt'])
